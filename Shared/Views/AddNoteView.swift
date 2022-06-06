09dfec200d98719da6979bbaeef3f53e0c2c2fa0
Mahesh Sai <maheshsai252@gmail.com>
2022-06-06 23:53:42 +0530

    Add Note change

--- Shared/Views/AddNoteView.swift
+++ Shared/Views/AddNoteView.swift
@@ -130,13 +130,3 @@ struct AddNoteView: View {
 //        AddNoteView()
 //    }
 //}
-extension String {
-    func numberOfLines() -> Int {
-        return self.numberOfOccurrencesOf(string: "\n") + 1
-    }
-    
-    func numberOfOccurrencesOf(string: String) -> Int {
-        return self.components(separatedBy:string).count - 1
-    }
-    
-}
